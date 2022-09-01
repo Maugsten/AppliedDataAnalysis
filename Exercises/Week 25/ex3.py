70ba8a166a6f346e3bf340dfad7b643606463f43
celinems <celinems@uio.no>
2022-09-01 09:54:20 +0200

    Added MSE and R2 calculation

--- Exercises/Week 25/ex3.py
+++ Exercises/Week 25/ex3.py
@@ -35,29 +35,59 @@ X_test_scaled = scale(X_test)
 x_train_scaled = scale(x_train)
 x_test_scaled = scale(x_test)
 y_train_scaled = scale(y_train)
+y_test_scaled = scale(y_test)
 
 # ordinary least squares
 def OLS(x_data, y_data):
     clf2 = LinearRegression(fit_intercept=False)
     clf2.fit(x_data,y_data)
     y_predict = clf2.predict(x_data)
+    weights = clf2.coef_
 
-    return y_predict
+    return y_predict, weights
 
-y_predict = OLS(X_train, y_train)
-y_predict_scaled = OLS(X_train_scaled, y_train_scaled)
+y_predict, weights = OLS(X_train, y_train)
+y_predict_scaled, weights_scaled = OLS(X_train_scaled, y_train_scaled)
 
+def MSE_R2(y,y_predict):
+    mean_sq_err = mean_squared_error(y, y_predict)
+    det_coeff = r2_score(y, y_predict)
+
+    return mean_sq_err, det_coeff
+
+MSE_train, R2_train = MSE_R2(y_train, y_predict)
+MSE_train_scaled, R2_train_scaled = MSE_R2(y_train_scaled, y_predict_scaled)
 
 plt.figure("Figure 1")
 plt.title("Original data")
 plt.scatter(x_train, y_predict, label='Fit')
-plt.scatter(x_train, y_train, label='Data', color='orange', s=15)
+plt.scatter(x_train, y_train, label='Training Data', color='orange', s=15)
 plt.legend()
+textstr1 = f"MSE = {MSE_train:.3f} \nR2 = {R2_train:.3f}"
+plt.text(1.5, 0.2, textstr1, size=12,
+         ha="center", va="center",  # horisontal alignment, vertical alignment
+         bbox=dict(boxstyle = "round",
+                   fc = "lemonchiffon", # facecolor
+                   alpha = 0.5,  # transparancy
+                   )
+         )
 plt.show()
 
 plt.figure("Figure 2")
 plt.title("Scaled data")
 plt.scatter(x_train_scaled, y_predict_scaled, label='Fit')
-plt.scatter(x_train_scaled, y_train_scaled, label='Data', color='orange', s=15)
+plt.scatter(x_train_scaled, y_train_scaled, label='Training Data', color='orange', s=15)
 plt.legend()
+textstr2 = f"MSE = {MSE_train_scaled:.3f} \nR2 = {R2_train_scaled:.3f}"
+plt.text(0.75, -1, textstr2, size=12,
+         ha="center", va="center",
+         bbox=dict(boxstyle="round",
+                   fc = "lemonchiffon",
+                   alpha = 0.5,
+                   )
+         )
 plt.show()
+
+""""
+For the test data we need to get the acutal weights from scikit. 
+"""
