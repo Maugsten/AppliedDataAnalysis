60e2511ebef0a4a43b75bf9c0f9724a024efb94c
Maugsten <olemaugsten@gmail.com>
2022-09-22 06:59:34 +0200

    added bootstrapping

--- Project1/Franke-function.py
+++ Project1/Franke-function.py
@@ -1,5 +1,6 @@
 
 # from calendar import c
+from operator import index
 from numpy import full
 from mpl_toolkits.mplot3d import Axes3D
 import matplotlib.pyplot as plt
@@ -11,15 +12,15 @@ from sklearn.model_selection import train_test_split
 from IPython import embed
 
 
-def FrankeFunction(x,y):
+def FrankeFunction(x, y):
     """
     Description:
         An implementation of the Franke function.
     Input:
-        x (numpy array): Mesh for x values.
-        y (numpy array): Mesh for y values.
+        x (numpy array): Mesh with x values.
+        y (numpy array): Mesh with y values.
     Output:
-        z (numpy array): The function values.
+        z (numpy array): Mesh with function values.
     """
     term1 = 0.75*np.exp(-(0.25*(9*x-2)**2) - 0.25*((9*y-2)**2))
     term2 = 0.75*np.exp(-((9*x+1)**2)/49.0 - 0.1*(9*y+1))
@@ -46,85 +47,117 @@ def create_X(x, y, n):
 
     N = len(x)
     l = int((n+1)*(n+2)/2)  # Number of elements in beta
-    X = np.ones((N,l))
+    X = np.ones((N, l))
 
-    for i in range(1,n+1):
-    	q = int((i)*(i+1)/2)
-    	for k in range(i+1):
-    		X[:,q+k] = (x**(i-k))*(y**k)
+    for i in range(1, n+1):
+        q = int((i)*(i+1)/2)
+        for k in range(i+1):
+            X[:, q+k] = (x**(i-k))*(y**k)
 
     return X
 
 
-def singular_value_decomp(X, z):
+def svd_algorithm(X, z):
     """
+    Description:
+        Uses SVD-decomposition of the design matrix and the 
+        Moore-Penrose pseudoinverse to calculate the optimal 
+        parameters beta. Also uses SVD to calculate the variance 
+        of each parameter.
     Input: 
-        * X - design matrix
-        * z_ - data we want to fit
-
+        X (numpy array): Design matrix
+        z (numpy array): 1D-array for the data we want to fit
     Return:
-        * z_tilde - predicted values
-        * betas - weights to apply on test data
+        betas (numpy array): Optimal parameters beta
+        betas_variance (numpy array): Variance of each parameters beta
     """
+    
     U, Sigma, Vt = np.linalg.svd(X, full_matrices=False)
-    z_tilde = U @ U.transpose() @ z  
-    betas =  np.linalg.pinv(X.transpose() @ X) @ X.transpose() @ z  # this is not the SVD way
-    # betas_variance = sigma**2 * np.linalg.pinv(Vt.transpose() @ Sigma @ Sigma @ Vt)
-
+    betas = Vt.transpose() @ np.diag(1/Sigma) @ U.transpose() @ z
     betas_variance = sigma**2 * np.linalg.pinv(Vt.transpose() @ np.diag(Sigma) @ np.diag(Sigma) @ Vt)
-    return z_tilde, betas, betas_variance
+    betas_variance = np.diag(betas_variance)
+
+    return betas, betas_variance
 
 
-if __name__ == "__main__":
-    np.random.seed(1) # Set seed so results can be reproduced.
-
-    # Define domain. No need to scale this data as it's already in the range (0,1)
-    x = np.arange(0, 1, 0.05)
-    y = np.arange(0, 1, 0.05)
-    x, y = np.meshgrid(x,y)
-
-    sigma = .1 # Standard deviation of the noise
-    z = FrankeFunction(x, y) + np.random.normal(0, sigma, x.shape)  # Franke function with stochastic noise
+def ordinary_least_squares(x, y, z, polydeg=5, resampling='None'):
+    """
+    Description:
+        Preforms an ordinary least squares regression on the data z. 
+        We assume that the data z might depend on x and y.
+        The features in the design matrix are polynomials of x and y.
+        The OLS regression can be done with bootstraping or cross-validation. 
+    Input:
+        x (numpy array): Mesh with x values.
+        y (numpy array): Mesh with y values.
+        z (numpy array): Mesh with z values. This is the data we try to make a fit for.
+        polydeg (int): Highest order of polynomial degree in the design matrix.
+        resampling (string): 
+            Use 'None' for OLS without resampling.
+            Use 'Bootstrap' for OLS with bootstrapping.
+            Use 'CrossValidation' for OLS with cross-validation.
+    Output:
+        Plots
+    """
 
     # Format data
-    z_ = z.flatten().reshape(-1,1)
+    z_ = z.flatten().reshape(-1, 1)
     x_ = x.flatten()
     y_ = y.flatten()
 
-    """ 
-    NB! Jeg foreslår å pakke mye av det som kommer under her i en funksjon for OLS regresjon 
-    """
-
-    # Set order of polynomials for design matrix
+    # Set the first order of polynomials for design matrix to be looped from
     startdeg = 1
-    polydeg = 5
 
-    # Make arrays for MSEs and R2 scores 
+    # Make arrays for MSEs and R2 scores
     MSE_train = np.zeros(polydeg-startdeg+1)
     MSE_test = np.zeros(polydeg-startdeg+1)
     R2_train = np.zeros(polydeg-startdeg+1)
     R2_test = np.zeros(polydeg-startdeg+1)
 
     # Make lists for parameters and variance
-    collected_betas = [] 
+    collected_betas = []
     collected_betas_variance = []
 
     # Loop for OLS with increasing order of polynomial fit
-    for i in range(startdeg,polydeg+1):
+    for i in range(startdeg, polydeg+1):
 
         # Make design matrix
-        X = create_X(x,y,i)  
+        X = create_X(x, y, i)
 
         # Split into train and test data
-        X_train, X_test, z_train, z_test, x_train, x_test, y_train, y_test = train_test_split(X,z_,x_,y_,test_size=0.2)
+        X_train, X_test, z_train, z_test = train_test_split(X, z_, test_size=0.2)
 
-        # OLS with SVD
-        z_tilde_train, betas, betas_variance = singular_value_decomp(X_train, z_train)
+        if resampling == "Bootstrap":
+            beta = np.zeros((len(X_train[0]), 1000)) # 1000 is number of bootstraps
+
+            for j in range(1000): # For each bootstrap
+                # Pick new randomly sampled training data and matching design matrix
+                indx = np.random.randint(0,100,1000)
+                X_train_btstrp = X_train[indx]
+                z_train_btstrp = z_train[indx]
+
+                # Calculate betas for this bootstrap
+                beta[:,j] = svd_algorithm(X_train_btstrp, z_train_btstrp)[0].flatten()  
+
+            # Average parameters over the bootstraps
+            betas_averaged = np.mean(beta, axis=1)
+            betas = np.zeros((len(X_train[0]),1))
+            betas[:,0] = betas_averaged
+
+            # Calculate variances of the parameters
+            betas_variance = np.std(beta, axis=1)
+
+        else:
+            # OLS with SVD
+            betas, betas_variance = svd_algorithm(X_train, z_train)
+
+        # Calculate training fit and test fit
+        z_tilde_train = X_train @ betas
         z_tilde_test = X_test @ betas
 
         # Collect parameters
         collected_betas.append(betas)
-        collected_betas_variance.append(np.diag(betas_variance))
+        collected_betas_variance.append(betas_variance)
 
         # Calculate mean square errors
         MSE_train[i-startdeg] = np.sum((z_train - z_tilde_train)**2)/len(z_train)
@@ -137,24 +170,23 @@ if __name__ == "__main__":
         R2_train[i-startdeg] = 1 - np.sum((z_train - z_tilde_train)**2)/np.sum((z_train - z_mean)**2)
         R2_test[i-startdeg] = 1 - np.sum((z_test - z_tilde_test)**2)/np.sum((z_test - z_mean)**2)
 
-
     # Calculate predicted values using all data (X)
-    z_tilde = singular_value_decomp(X, z_)[0]
+    z_tilde = X @ svd_algorithm(X, z_)[0]
 
     # Get the right shape for plotting
-    z_ = z_.reshape((20,20))
-    z_tilde = z_tilde.reshape((20,20))
+    z_ = z_.reshape((len(x), len(x)))
+    z_tilde = z_tilde.reshape((len(x), len(x)))
 
+    z_tilde_train = X @ betas
 
     # Plots of the surfaces.
     fig = plt.figure(figsize=plt.figaspect(0.5), constrained_layout=True)
     ax1 = fig.add_subplot(1, 2, 1, projection='3d')
     ax2 = fig.add_subplot(1, 2, 2, projection='3d')
     surf = ax1.plot_surface(x, y, z_, cmap=cm.coolwarm,
-                        linewidth=0, antialiased=False)
+                            linewidth=0, antialiased=False)
     surf1 = ax2.plot_surface(x, y, z_tilde, cmap=cm.coolwarm,
-                        linewidth=0, antialiased=False)              
-
+                             linewidth=0, antialiased=False)
     ax1.set_zlim(-0.10, 1.40)
     ax1.zaxis.set_major_locator(LinearLocator(10))
     ax1.zaxis.set_major_formatter(FormatStrFormatter('%.02f'))
@@ -163,12 +195,11 @@ if __name__ == "__main__":
     ax2.zaxis.set_major_locator(LinearLocator(10))
     ax2.zaxis.set_major_formatter(FormatStrFormatter('%.02f'))
     ax2.set_title("OLS fit")
-    fig.colorbar(surf, shrink=0.5, aspect=10) # Add a color bar which maps values to colors.
+    fig.colorbar(surf, shrink=0.5, aspect=10)
     plt.show()
 
-
     # Plot of the MSEs
-    x_axis = np.linspace(startdeg,polydeg,polydeg-startdeg+1)
+    x_axis = np.linspace(startdeg, polydeg, polydeg-startdeg+1)
     plt.plot(x_axis, MSE_train, label="Training Sample")
     plt.plot(x_axis, MSE_test, label="Test Sample")
     plt.title("MSE vs Complexity")
@@ -176,7 +207,6 @@ if __name__ == "__main__":
     plt.ylabel("Mean Square Error")
     plt.legend()
     plt.show()
-
 
     # Plot of the R2 scores
     plt.plot(x_axis, R2_train, label="Training Sample")
@@ -187,16 +217,35 @@ if __name__ == "__main__":
     plt.legend()
     plt.show()
 
-
     # Plotting paramaters and variance against terms
-    plt.figure(figsize=(6,4))
+    plt.figure(figsize=(6, 4))
     for i in range(len(collected_betas)):
-        plt.errorbar(range(1, len(collected_betas[i])+1), collected_betas[i].flatten(), collected_betas_variance[i], fmt='o', capsize=6)
+        plt.errorbar(range(1, len(collected_betas[i])+1), collected_betas[i].flatten(
+        ), collected_betas_variance[i], fmt='o', capsize=6)
     plt.legend(['Order 1', 'Order 2', 'Order 3', 'Order 4', 'Order 5'])
     plt.title('Optimal parameters beta')
     plt.ylabel('Beta value []')
     plt.xlabel('Term index')
     plt.show()
+
+
+if __name__ == "__main__":
+    np.random.seed(69)  # Set seed so results can be reproduced.
+
+    # Define domain. No need to scale this data as it's already in the range (0,1)
+    x = np.arange(0, 1, 0.05)
+    y = np.arange(0, 1, 0.05)
+    x, y = np.meshgrid(x, y)
+
+    sigma = .1  # Standard deviation of the noise
+    
+    # Franke function with stochastic noise
+    z = FrankeFunction(x, y) + np.random.normal(0, sigma, x.shape)
+
+    # Preform regression
+    ordinary_least_squares(x, y, z, polydeg=5, resampling='None')
+    ordinary_least_squares(x, y, z, polydeg=5, resampling='Bootstrap')
+
 
     """
     Terminal>>>python filename.py
