111d7eb776a7fe83e51d9661f5e516bac5765681
Celine Marie Solberg <celinems@uio.no>
2022-10-04 12:50:48 +0200

    added real data

--- Project1/real-data.py
+++ Project1/real-data.py
@@ -5,10 +5,13 @@ import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 from matplotlib import cm 
 from functions import *
+from random import random, seed
+
 
 # load the terrain data
 terrain = imread('SRTM_data_Norway_2.tif')
-terrain = terrain / np.amax(terrain)
+terrain = terrain[::100, ::100] / np.amax(terrain)
+np.random.seed(1999)
 
 # # plot the terrain
 # plt.figure()
@@ -24,5 +27,6 @@ y = np.linspace(0,len(terrain[:,1]),len(terrain[:,1]))
 x,y = np.meshgrid(x,y)
 
 polydeg=5
-ordinary_least_squares(x,y,terrain,polydeg)
+# ordinary_least_squares(x,y,terrain,polydeg)
+ordinary_least_squares(x, y, terrain, polydeg, resampling='CrossValidation')
 
